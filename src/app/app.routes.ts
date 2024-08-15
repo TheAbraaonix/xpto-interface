921fd24d947cc3c67440eadc6dc3631fee506c4e
Carlos Abraão <carlosabraao@hotmail.com.br>
2024-08-15 10:22:31 -0300

    creating create service order component and implementing navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CreateServiceOrderComponent } from './create-service-order/create-service-order.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", component: HomeComponent},
+    {path: "create-service-order", component: CreateServiceOrderComponent}
+];
---
+++ src/app/create-service-order/create-service-order.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-service-order',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-service-order.component.html',
+  styleUrl: './create-service-order.component.css'
+})
+export class CreateServiceOrderComponent {
+
+}
